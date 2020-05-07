8d403842023efdcceaab56e7b5ceed35c1c5dd5f
Vinicius <viniciusvks@gmail.com>
2020-05-06 21:43:45 -0300

    Versao inicial

--- src/main/java/br/fundatec/lp3/designpatterns/simplefactory/pizzaria/Pizza.java
+++ src/main/java/br/fundatec/lp3/designpatterns/simplefactory/pizzaria/Pizza.java
@@ -1,21 +1,21 @@
 package br.fundatec.lp3.designpatterns.simplefactory.pizzaria;
 
-public abstract class Pizza {
+public class Pizza {
 
-	public abstract void preparar();
-	
+	public void preparar() {
+		System.out.println("Preparando Pizza");
+	}
+
 	public void cozinhar() {
 		System.out.println("Assando por 25 min. a 350 graus");
 	}
-	
-	public void cortar () {
+
+	public void cortar() {
 		System.out.println("Cortando em fatias diagonais");
 	}
-	
+
 	public void embalar() {
 		System.out.println("Acomodando na caixa da pizzaria");
 	}
-	
-	public abstract String getSabor();
-	
+
 }
--- src/main/java/br/fundatec/lp3/designpatterns/simplefactory/pizzaria/Pizzaria.java
+++ src/main/java/br/fundatec/lp3/designpatterns/simplefactory/pizzaria/Pizzaria.java
@@ -1,22 +1,19 @@
 package br.fundatec.lp3.designpatterns.simplefactory.pizzaria;
 
 public class Pizzaria {
-	
-	private FactoryDePizzaSimples factory;
-	
-	public Pizzaria(FactoryDePizzaSimples factory) {
-		this.factory = factory;
+
+	public Pizzaria() {
 	}
-	
-	public Pizza pedirPizza(String sabor) {
-		
-		Pizza pizza = factory.criarPizza(sabor);
-		
+
+	public Pizza pedirPizza() {
+
+		Pizza pizza = new Pizza();
+
 		pizza.preparar();
 		pizza.cozinhar();
 		pizza.cortar();
 		pizza.embalar();
-		
+
 		return pizza;
 	}
 
